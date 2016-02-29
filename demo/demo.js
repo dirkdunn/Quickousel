605c1946e6597beb4e848c964e29a8b1a043a73c
Dirk <dirkdunn91@gmail.com>
2016-02-29 07:17:18 -0500

    refactor architecture, create demo, add arrows, custom buttons, custom html, refactor code

---
+++ demo/demo.js
@@ -0,0 +1,75 @@
+// Handler's for the demo.
+(function(){
+	var theObject = {
+		transitionStyle: 'fadeIn',
+		transitionSpeed: 8000,
+		arrows: false,
+		button: 'dottedsquare'
+
+	},
+	setStyle = function(){
+		theObject['transitionStyle'] = $(this).val();
+		createObject();
+	},
+	setSpeed = function(){
+		theObject['transitionSpeed'] = eval($(this).val());
+		createObject();
+	},
+	setArrows = function(){
+		theObject['arrows'] = eval($(this).val());
+		createObject();
+	},
+	setButtons = function(){
+		theObject['button'] = $(this).val();
+		createObject();
+	};
+
+	function setHandlers(){
+		$('.transitionSpeed').on('change',function(){
+			setSpeed.call(this);
+		});
+
+		$('.transitionStyle').on('change',function(){
+			setStyle.call(this);
+		});
+
+		$('.arrows').on('change',function(){
+			setArrows.call(this);
+		});
+
+		$('.buttonStyle').on('change',function(){
+			setButtons.call(this);
+		});
+
+		// add to lib?
+		$('#quickousel').on('resize',function(){
+			// if( window.innerWidth < 768){
+				var carouselButtons = $('#quickousel .carousel-buttons');
+				var buttonAdjust = '-' + String(parseInt( /\d+/.exec(carouselButtons.css('width')) ) / 2) + 'px'
+				carouselButtons.css('margin-left',buttonAdjust);
+			// }
+		});
+	};
+
+	function createObject(){
+		var stringObj = JSON.stringify(theObject)
+		.replace(/,/g,',<br>')
+		.replace(/^\s*{/g,"{<br>")
+		.replace(/\}$/g,function(match){ 
+			return '<br>' + match;
+		})
+		
+		$('#formDemo').find('span.right.json code').html(
+			['// Copy && Paste <br>Quickousel.init(',stringObj, ');'].join(""))
+
+		Quickousel.init(theObject);
+	};
+
+	function init(){
+		setHandlers();
+		Quickousel.init(theObject);
+
+	}
+
+	window.onload = init;
+})()
